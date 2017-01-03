80346f39da0163a7d9d53c303d895b3149bdaac9
Alessandra Messineo <alessandramessineo@Alessandras-MacBook-Air-2.local>
2017-01-03 16:47:42 -0500

    updated to create cards using JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,23 @@ var card3 = "queen";
 var card4 = "king";
 
 
-if (card1 === card3){
+/* if (card1 === card3){
 	alert("You found a match!")
 } else {
 	alert("Sorry, not a match!")
-}
+} 
+*/
+
+var board = document.getElementById("gameboard");
+ 
+ var createCards = function(){
+	for (var i = 1; i<7; i++){
+ 		var card = document.createElement('div');
+ 		card.className = "card";
+ 		gameboard.appendChild(card);
+ 	}
+ }
+
+ createCards()
+
+ 
