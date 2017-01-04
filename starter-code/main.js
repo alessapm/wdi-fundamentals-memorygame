1f145b74adbc92b91d5d4650f2b6f343f7e2e606
Alessandra Messineo <alessandramessineo@Alessandras-MacBook-Air-2.local>
2017-01-04 15:11:49 -0500

    changed for unit 11 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -13,14 +13,39 @@ var card4 = "king";
 
 var board = document.getElementById("gameboard");
  
- var createCards = function(){
-	for (var i = 1; i<7; i++){
- 		var card = document.createElement('div');
- 		card.className = "card";
- 		gameboard.appendChild(card);
+ // var createCards = function(){
+	// for (var i = 1; i<10; i++){
+ // 		var card = document.createElement('div');
+ // 		card.className = "card";
+ // 		gameboard.appendChild(card);
+ // 	}
+ // }
+
+ // createCards()
+
+ var cards = ["queen", "queen", "king", "king"];
+
+ var cardsInPlay = [];
+
+ var isTwoCards = function(){
+ 	cardsInPlay.push(this.getAttribute("data-card"));
+ 	if (cardsInPlay.length === 2){
+ 		isMatch(cardsInPlay);
+ 		cardsInPlay = [];
  	}
- }
+ };
 
- createCards()
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++){
+		cardElement.setAttribute("data-card", cards[i]);
+		cardElement.addEventListener("click", isTwoCards);
+		cardElement.innerHTML = '<img src = "king-card.png" alt="King" />'
+	}
+}
 
- 
+var isMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		cardElement.innerHTML = '';
+	}
+}
+
